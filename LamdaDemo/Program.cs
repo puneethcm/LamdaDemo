eb3c720e896ae488aec374a10041d1acc1efa832
puneethcm <puneethkumarcm8050@gmail.com>
2023-04-13 16:18:59 +0530

    student details

--- LamdaDemo/Program.cs
+++ LamdaDemo/Program.cs
@@ -1,8 +1,27 @@
-﻿internal class Program
+﻿using LamdaDemo;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Lamda Expression");
+        Console.WriteLine();
+        List<Student> list = new List<Student>()
+        {
+            new Student(){Id=1,Name="Puneeth",PhoneNumber=123456,Age=24,Address="Davngere"},
+            new Student(){Id=2,Name="Abhi",PhoneNumber=2875212,Age=23,Address="Banglore"},
+            new Student(){Id=3,Name="Pavan",PhoneNumber=2141241,Age=22,Address="Manglore"},
+            new Student(){Id=4,Name="Swathi",PhoneNumber=21412141,Age=20,Address="Hubli"},
+        };
+        DisplayStudentDetais(list);
         Console.ReadLine();
+    }
+
+    public static void DisplayStudentDetais(List<Student> list)
+    {
+        foreach(Student student in list)
+        {
+            Console.WriteLine(student.ToString());
+        }
     }
 }
---
+++ LamdaDemo/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LamdaDemo
+{
+	public class Student
+	{
+		public int Id { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Id:{Id} Name:{Name} Age:{Age} Address:{Address} PhoneNumber:{PhoneNumber}";
+        }
+    }
+}
+
